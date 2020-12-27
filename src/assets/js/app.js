af3006776b7e8b3fb187cc5209ba736c8058b6e9
unknown <denislavgavrilov99@gmail.com>
2020-12-27 18:34:11 +0200

    minor change

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,4 @@
-let socket = io.connect('http://localhost:3000', {
+let socket = io.connect('https://devtectives.asciinaut.sh', {
     reconnection: false,
     transports: ['websocket'],
     upgrade: false
